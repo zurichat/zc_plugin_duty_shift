fcea18e7a06348bf1f3267e5477a0815d98711aa
Eyimofe Omotayo <eyimofeomotayo@gmail.com>
2021-09-01 13:58:12 +0100

    Calendar view from mofe

--- App/ClientApp/src/components/Layout.js
+++ App/ClientApp/src/components/Layout.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import { Container } from 'reactstrap';
 import { NavMenu } from './NavMenu';
 
 export class Layout extends Component {
   static displayName = Layout.name;
 
-  render () {
+  render() {
     return (
       <div>
         <NavMenu />
-        <Container>
-          {this.props.children}
-        </Container>
+        {/* <Container fluid={true} > */}
+
+        {this.props.children}
+        {/* </Container> */}
       </div>
     );
   }
--- App/ClientApp/src/components/NavMenu.js
+++ App/ClientApp/src/components/NavMenu.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink, Nav } from 'reactstrap';
+
+
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
+import DropdownCustom from './customs/Dropdown';
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
 
-  constructor (props) {
+  constructor(props) {
     super(props);
 
     this.toggleNavbar = this.toggleNavbar.bind(this);
@@ -15,29 +18,41 @@ export class NavMenu extends Component {
     };
   }
 
-  toggleNavbar () {
+  toggleNavbar() {
     this.setState({
       collapsed: !this.state.collapsed
     });
   }
 
-  render () {
+  render() {
     return (
-      <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
+      <header style={{ marginBottom: '-17px', }}>
+        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" style={{ backgroundColor: '#00B87C', height: '40px' }}>
           <Container>
-            <NavbarBrand tag={Link} to="/">App</NavbarBrand>
+            <NavbarBrand tag={Link} to="/" ><h5 style={{ color: '#fff' }}>Team Tolstoy-.NET/Plugin</h5></NavbarBrand>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink tag={Link} className="text-light" to="/"> Schedule</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} className="text-light" to="/"> Request</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} className="text-light" to="/"> Settings</NavLink>
+              </NavItem>
+            </Nav>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/">
+
+
+                    <DropdownCustom />
+                  </NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                  <NavLink tag={Link} className="text-light" to="/"> Share with team</NavLink>
                 </NavItem>
               </ul>
             </Collapse>

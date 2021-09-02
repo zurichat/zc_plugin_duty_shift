edca932d23e7a39555c3a3d2ed7b13d5b341907c
abisoyeola <olaabisoye@gmail.com>
2021-09-02 11:19:35 +0100

    Added Shift Controller With Dummy Data
    Added A controller for The Shift,
    User can now Get List of the Shift

---
+++ App/Models/Shift.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Models
+{
+    public class Shift
+    {
+        public long ID { get; set; }
+        public string ShiftName { get; set; }
+        public string TimeStart { get; set; }
+        public string TimeEnd { get; set; }
+        public int DepartmentID { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}

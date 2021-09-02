96693e6b19dd0c361f301e629a6c4066f520a9e0
abisoyeola <olaabisoye@gmail.com>
2021-09-02 13:57:55 +0100

    Installing Plugin
    Installing Plugin

---
+++ App/Models/UserTypes.cs
@@ -0,0 +1,8 @@
+namespace App.Models
+{
+    public class UserTypes
+    {
+        public int Id {get; set;}
+        public string Types {get; set;}
+    }
+}
---
+++ App/Models/Users.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string ZuriUserId { get; set; }
+        public UserTypes UserTypeId { get; set; }
+        public DateTime TimeJoined { get; set; }
+    }
+}

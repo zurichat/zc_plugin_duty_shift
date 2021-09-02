95e0d8ddcd75b49dfb40ff33b38953c44987f1e6
abisoyeola <olaabisoye@gmail.com>
2021-09-02 14:53:57 +0100

    Update Shift Controller
    Update Shift Controller, Added 4 models, Shift, ShiftSchedule,user,usertypes

---
+++ App/Models/Users.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        [Required]
+        public string ZuriUserId { get; set; }
+        [Required]
+        public UserTypes UserTypeId { get; set; }
+        [Required]
+        public int InvitedById { get; set; }
+        [Required]
+        public DateTime TimeJoined { get; set; }
+    }
+}

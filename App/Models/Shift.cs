a54eab01ea851ad6f81c384469fb79db57285ea9
abisoyeola <olaabisoye@gmail.com>
2021-09-02 15:06:40 +0100

    Added Models

---
+++ App/Models/Shift.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Models
+{
+    public class Shift
+    {
+        public long Id { get; set; }
+        [Required]
+        public string ShiftTitle { get; set; }
+        [Required]
+        public string TimeStart { get; set; }
+        [Required]
+        public string TimeEnd { get; set; }
+        [Required]
+        public int LeadById { get; set; }
+        [Required]
+        public int CreatedById { get; set; }
+        [Required]
+        public DateTime DateCreated { get; set; }
+    }
+}

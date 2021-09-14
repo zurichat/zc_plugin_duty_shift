7d581647b748aa71a8748b6901e4dcf9d04bbf69
Babatunde Adenowo <mo.xodeeq@gmail.com>
2021-09-14 14:09:35 +0100

    Delete and Success dialogs starter templates

--- App/ClientApp/src/components/PopupModal.js
+++ App/ClientApp/src/components/PopupModal.js
@@ -9,115 +9,112 @@ import {
 
 function PopupModal({ mode, setModalStatus }) {
   return (
-    <div className="fixed inset-0 z-50 flex w-full h-full overflow-auto bg-gray-900 bg-opacity-50 pin bg-blend-darken lg:p-28">
-      
-      <section className="relative flex flex-col m-auto overscroll-contain">
+    <>
+      <header className="flex justify-between p-4 mx-2 text-center text-white bg-green-500 rounded-t-xl lg:rounded-t-2xl">
+        <h2 className="my-auto text-base lg:mr-10 lg:ml-3">
+          {mode === "new" ? "Add new shift" : "Edit shift"}
+        </h2>
+        <XCircleIcon
+          onClick={() => setModalStatus(null)}
+          className="w-6 h-6 stroke-current stroke-1 lg:mr-3 lg:w-9 lg:h-9 lg:ml-60"
+        />
+      </header>
 
-        <header className="flex justify-between p-4 mx-2 text-center text-white bg-green-500 rounded-t-xl lg:rounded-t-2xl">
-          <h2 className="my-auto text-base lg:mr-10 lg:ml-3">
-            {mode === "new" ? "Add new shift" : "Edit shift"}
-          </h2>
-          <XCircleIcon
-            onClick={() => setModalStatus(null)}
-            className="w-6 h-6 stroke-current stroke-1 lg:mr-3 lg:w-9 lg:h-9 lg:ml-60"
+      <form
+        action=""
+        className="grid grid-cols-2 gap-6 p-5 mx-2 overflow-hidden bg-white mb-15 rounded-b-xl lg:rounded-b-2xl"
+      >
+        <div className="lg:relative lg:mt-4">
+          <label
+            htmlFor="date"
+            className="flex items-center mb-3 text-gray-400"
+          >
+            <CalendarIcon className="w-6 h-6 lg:hidden" />
+            <span className="ml-2 text-xs lg:text-base">Date</span>
+          </label>
+          <input
+            type="text"
+            id="date"
+            className="p-3 font-sans text-base bg-gray-100 rounded-md outline-none lg:text-base w-100 lg:w-72 lg:ml-2"
+            value="Mon, August 28, 2021"
           />
-        </header>
+          <CalendarIcon className="hidden w-6 h-6 text-gray-400 lg:absolute lg:inline lg:bottom-4 lg:right-8" />
+        </div>
 
-        <form
-          action=""
-          className="grid grid-cols-2 gap-6 p-5 mx-2 overflow-hidden bg-white mb-15 rounded-b-xl lg:rounded-b-2xl"
-        >
-          <div className="lg:relative lg:mt-4">
-            <label
-              htmlFor="date"
-              className="flex items-center mb-3 text-gray-400"
-            >
-              <CalendarIcon className="w-6 h-6 lg:hidden" />
-              <span className="ml-2 text-xs lg:text-base">Date</span>
-            </label>
-            <input
-              type="text"
-              id="date"
-              className="p-3 font-sans text-base bg-gray-100 rounded-md outline-none lg:text-base w-100 lg:w-72 lg:ml-2"
-              value="Mon, August 28, 2021"
-            />
-            <CalendarIcon className="hidden w-6 h-6 text-gray-400 lg:absolute lg:inline lg:bottom-4 lg:right-8" />
-          </div>
+        <div className="relative lg:mt-4">
+          <label
+            htmlFor="date"
+            className="flex items-center mb-3 text-gray-400 lg:text-xl"
+          >
+            <span className="text-base lg:ml-2 lg:text-base">Shift</span>
+          </label>
+          <input
+            type="text"
+            id="date"
+            className="p-3 font-sans text-sm bg-gray-100 border rounded-md outline-none lg:text-base w-44 lg:w-72 lg:ml-2"
+            value="Morning Shift"
+          />
+          <ChevronDownIcon className="absolute w-6 h-6 text-gray-400 bottom-4 right-6 lg:bottom-4 lg:right-8" />
+        </div>
 
-          <div className="relative lg:mt-4">
-            <label
-              htmlFor="date"
-              className="flex items-center mb-3 text-gray-400 lg:text-xl"
-            >
-              <span className="text-base lg:ml-2 lg:text-base">Shift</span>
-            </label>
-            <input
-              type="text"
-              id="date"
-              className="p-3 font-sans text-sm bg-gray-100 border rounded-md outline-none lg:text-base w-44 lg:w-72 lg:ml-2"
-              value="Morning Shift"
-            />
-            <ChevronDownIcon className="absolute w-6 h-6 text-gray-400 bottom-4 right-6 lg:bottom-4 lg:right-8" />
-          </div>
+        <div className="relative lg:mt-4">
+          <label
+            htmlFor="date"
+            className="flex items-center mb-3 text-gray-400 lg:text-xl"
+          >
+            <ClockIcon className="w-6 h-6 lg:hidden" />
+            <span className="ml-2 text-base lg:text-base">Time</span>
+          </label>
+          <input
+            type="text"
+            id="date"
+            className="p-3 font-sans text-sm bg-gray-100 rounded-md outline-none lg:text-base w-44 lg:w-72 lg:ml-2"
+            value="09:00am"
+          />
+          <ClockIcon className="hidden w-6 h-6 text-gray-400 lg:absolute lg:inline lg:bottom-6 lg:right-8" />
+        </div>
 
-          <div className="relative lg:mt-4">
-            <label
-              htmlFor="date"
-              className="flex items-center mb-3 text-gray-400 lg:text-xl"
-            >
-              <ClockIcon className="w-6 h-6 lg:hidden" />
-              <span className="ml-2 text-base lg:text-base">Time</span>
-            </label>
-            <input
-              type="text"
-              id="date"
-              className="p-3 font-sans text-sm bg-gray-100 rounded-md outline-none lg:text-base w-44 lg:w-72 lg:ml-2"
-              value="09:00am"
-            />
-            <ClockIcon className="hidden w-6 h-6 text-gray-400 lg:absolute lg:inline lg:bottom-6 lg:right-8" />
-          </div>
+        <div className="relative lg:mt-4">
+          <label
+            htmlFor="date"
+            className="flex items-center mb-3 text-gray-400 lg:text-base"
+          >
+            <span className="text-base lg:ml-2 lg:text-base">End</span>
+          </label>
+          <input
+            type="text"
+            id="date"
+            className="p-3 font-sans text-base bg-gray-100 rounded-md outline-none lg:text-base w-44 lg:w-72 lg:ml-2"
+            value="12:00am"
+          />
+          <ClockIcon className="hidden w-6 h-6 text-gray-400 lg:absolute lg:inline lg:bottom-4 lg:right-8" />
+        </div>
 
-          <div className="relative lg:mt-4">
-            <label
-              htmlFor="date"
-              className="flex items-center mb-3 text-gray-400 lg:text-base"
-            >
-              <span className="text-base lg:ml-2 lg:text-base">End</span>
-            </label>
-            <input
-              type="text"
-              id="date"
-              className="p-3 font-sans text-base bg-gray-100 rounded-md outline-none lg:text-base w-44 lg:w-72 lg:ml-2"
-              value="12:00am"
-            />
-            <ClockIcon className="hidden w-6 h-6 text-gray-400 lg:absolute lg:inline lg:bottom-4 lg:right-8" />
-          </div>
+        <div className="relative lg:mt-4">
+          <label
+            htmlFor="date"
+            className="flex items-center mb-3 text-gray-400 lg:text-xl"
+          >
+            <span className="text-base lg:ml-2 lg:text-base">Title</span>
+          </label>
+          <input
+            type="text"
+            id="date"
+            className="p-3 font-sans text-sm bg-gray-100 border rounded-md outline-none lg:text-base w-44 lg:w-72 lg:ml-2"
+            value="Morning Shift"
+          />
+          <ChevronDownIcon className="absolute w-6 h-6 text-gray-400 bottom-4 right-6" />
+        </div>
 
-          <div className="relative lg:mt-4">
-            <label
-              htmlFor="date"
-              className="flex items-center mb-3 text-gray-400 lg:text-xl"
-            >
-              <span className="text-base lg:ml-2 lg:text-base">Title</span>
-            </label>
-            <input
-              type="text"
-              id="date"
-              className="p-3 font-sans text-sm bg-gray-100 border rounded-md outline-none lg:text-base w-44 lg:w-72 lg:ml-2"
-              value="Morning Shift"
-            />
-            <ChevronDownIcon className="absolute w-6 h-6 text-gray-400 bottom-4 right-6" />
-          </div>
-
-          <div className="items-center justify-between hidden p-3 font-sans text-sm bg-gray-100 rounded-md lg:flex lg:self-end lg:text-lg w-44 lg:w-72 lg:h-1/2 lg:ml-2">
-            <img
-              src="https://res.cloudinary.com/moh-x/image/upload/v1618542907/fcc/portfolio/AvatarMaker_ipxz90.svg"
-              alt="user's avatar"
-              className="w-8 h-8 border border-green-500 rounded-full xl:border-4"
-            />
-            <p className="text-base font-extralight">Babatunde Xodeeq</p>
-            <XIcon className="w-3 h-3" />
-          </div>
+        <div className="items-center justify-between hidden p-3 font-sans text-sm bg-gray-100 rounded-md lg:flex lg:self-end lg:text-lg w-44 lg:w-72 lg:h-1/2 lg:ml-2">
+          <img
+            src="https://res.cloudinary.com/moh-x/image/upload/v1618542907/fcc/portfolio/AvatarMaker_ipxz90.svg"
+            alt="user's avatar"
+            className="w-8 h-8 border border-green-500 rounded-full xl:border-4"
+          />
+          <p className="text-base font-extralight">Babatunde Xodeeq</p>
+          <XIcon className="w-3 h-3" />
+        </div>
 
         <div className="items-center justify-between hidden p-3 font-sans text-sm bg-gray-100 rounded-md lg:flex lg:self-end w-44 lg:w-70 lg:h-14 lg:text-base lg:ml-2">
           <img
@@ -129,22 +126,22 @@ function PopupModal({ mode, setModalStatus }) {
           <XIcon className="w-3 h-3" />
         </div>
 
-          <div className="relative lg:mt-4">
-            <label
-              htmlFor="date"
-              className="flex items-center mb-3 text-gray-400 lg:hidden lg:text-xl"
-            >
-              <span className="text-lg">Users</span>
-            </label>
-            <input
-              type="text"
-              id="date"
-              className="p-3 font-sans text-base bg-gray-100 rounded-md outline-none lg:text-xl w-44 lg:w-64 lg:ml-2 lg:bg-transparent lg:text-center lg:font-medium lg:placeholder-gray-400"
-              placeholder="Add Users"
-              readOnly
-            />
-            <UserAddIcon className="absolute w-6 h-6 text-gray-500 lg:w-9 lg:h-9 bottom-4 right-6 lg:bottom-4 lg:right-8" />
-          </div>
+        <div className="relative lg:mt-4">
+          <label
+            htmlFor="date"
+            className="flex items-center mb-3 text-gray-400 lg:hidden lg:text-xl"
+          >
+            <span className="text-lg">Users</span>
+          </label>
+          <input
+            type="text"
+            id="date"
+            className="p-3 font-sans text-base bg-gray-100 rounded-md outline-none lg:text-xl w-44 lg:w-64 lg:ml-2 lg:bg-transparent lg:text-center lg:font-medium lg:placeholder-gray-400"
+            placeholder="Add Users"
+            readOnly
+          />
+          <UserAddIcon className="absolute w-6 h-6 text-gray-500 lg:w-9 lg:h-9 bottom-4 right-6 lg:bottom-4 lg:right-8" />
+        </div>
 
         <div className="relative lg:mt-4 lg:text-base">
           <label
--- App/ClientApp/src/components/pages/EmployeeSchedule.js
+++ App/ClientApp/src/components/pages/EmployeeSchedule.js
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import { ChevronDownIcon } from "@heroicons/react/solid";
 import EmployeeShiftCard from "../EmployeeShiftCard";
 
 const EmployeeSchedule = () => {
-
   return (
     <main className="p-3 space-y-6 text-xs bg-gray-100 App lg:p-10 xl:p-12 md:text-sm lg:text-base xl:text-lg">
       <header className="flex justify-between">
@@ -12,19 +11,19 @@ const EmployeeSchedule = () => {
           <ChevronDownIcon className="w-6 h-6" />
         </button>
       </header>
-      <section className="space-y-6 w-full">
-        <header className="flex justify-between w-full px-3 py-3 font-medium text-white bg-green-500 rounded-sm xl:text-xl xl:font-semibold  xl:space-x-48 xl:items-start xl:pl-20"> 
+      <section className="w-full space-y-6">
+        <header className="flex justify-between w-full px-3 py-3 font-medium text-white bg-green-500 rounded-sm xl:text-xl xl:font-semibold xl:space-x-48 xl:items-start xl:pl-20">
           <h3 className="xl:pl-24">Staff Name</h3>
           <h3 className="hidden xl:pl-2 lg:block ">Post</h3>
           <h3 className="hidden xl:pl-2 lg:block">Lead</h3>
           <h3 className="text-right xl:text-left">Shift Time</h3>
         </header>
         <div className="space-y-6">
-          <EmployeeShiftCard  />
-          <EmployeeShiftCard  />
-          <EmployeeShiftCard  />
-          <EmployeeShiftCard  />
-          <EmployeeShiftCard  />
+          <EmployeeShiftCard />
+          <EmployeeShiftCard />
+          <EmployeeShiftCard />
+          <EmployeeShiftCard />
+          <EmployeeShiftCard />
         </div>
         <div className="flex xl:hidden">
           <button className="h-10 mx-auto text-sm text-center text-white bg-green-500 rounded-sm w-60">
@@ -33,7 +32,7 @@ const EmployeeSchedule = () => {
         </div>
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default EmployeeSchedule
+export default EmployeeSchedule;
